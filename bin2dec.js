b5b25091a71e28a982301c8fe1f6047d8bafbc06
Stephen <gstephenc@hotmail.com>
2023-06-13 17:48:22 -0400

    can convert binary to decimal now

--- bin2dec.js
+++ bin2dec.js
@@ -1,3 +1,17 @@
 function Bin2Dec(bin){
-    document.getElementById('output').innerHTML = "Decimal: 690";
+    var power = 0;
+    var result = 0;
+    for(var i = bin.length-1; i >= 0; i--){
+        var current = bin.charAt(i);
+        if(current == "0" || current == "1"){
+            result += Math.pow(2, power)*current;
+            power += 1;
+        }
+        else{
+            alert("Incorrect format");
+            result = "???";
+            break;
+        }
+    }
+    document.getElementById('output').innerHTML = "Decimal: " + result;
 }
